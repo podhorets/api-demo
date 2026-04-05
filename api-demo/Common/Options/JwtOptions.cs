51a3cb9ccb6f9fb2d7c2fc3c15855626ebeafed4
Dmytro Podhorets <podhorets@gmail.com>
2026-04-05 16:28:50 +0200

    add Jwt options

---
+++ api-demo/Common/Options/JwtOptions.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api_demo.Common.Options;
+
+public class JwtOptions
+{
+    [Required]
+    public string Secret { get; set; } = null!;
+    [Required]
+    public string Issuer { get; set; } = null!;
+    [Required]
+    public string Audience { get; set; } = null!;
+    [Required]
+    public int AccessTokenMinutes { get; set; }
+}
